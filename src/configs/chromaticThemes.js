9a122c8ea451e19dda39f4890938b04602bf281a
umeshadilshani <umeshadilshani723@gmail.com>
2025-02-07 13:11:33 +0530

    Configurable chromatic theme setup

--- src/configs/chromaticThemes.js
+++ src/configs/chromaticThemes.js
@@ -1,3 +1,4 @@
+// Setting configurable themes for dyslexic component
 const chromaticClasses = [['Chromatic Theme 1', 'chromTheme_1'], ['Chromatic Theme 2', 'chromTheme_2'], ['Chromatic Theme 3', 'chromTheme_3']];
 const colors = [
     { chromThemeColor_1: ['#F75827', '#05FF04', '#9473FE', '#C2A702', '#49AFFE'] },
